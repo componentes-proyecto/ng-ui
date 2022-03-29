0b8dd6444853450a8f87fc7828fe39b3be382462
Isaac Solano <isolanol@ucenfotec.ac.cr>
2022-03-28 19:29:10 -0600

    Base project

---
+++ src/app/header-nav/header-nav.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-header-nav',
+  templateUrl: './header-nav.component.html',
+  styleUrls: ['./header-nav.component.scss']
+})
+export class HeaderNavComponent implements OnInit {
+  public readonly menuItems: any[] = [
+    {
+      name: 'My Tasks',
+      route: '/tasks'
+    }, {
+      name: 'My Profile',
+      route: '/profile'
+    }
+  ];
+  public isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+  }
+  
+}
